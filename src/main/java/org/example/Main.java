877e87074a8917f1967219137a342298813454d3
kosiyyu <kosiyyu@gmail.com>
2022-12-08 01:54:00 +0100

    calculating HBC for example - works

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -76,6 +76,67 @@ public class Main {
         Container container = new Container(weightPoints, nodePoints);
         return container;
     }
+
+    public static Container calculateNodesAndWeights2(int numberOfIntegrationPoints) {
+        //nPoints - zmienna wyznaczajaca ilos pounktow calkowania
+
+        List<Double> nodes = new ArrayList<>();
+        List<Double> weights = new ArrayList<>();
+
+        if (numberOfIntegrationPoints == 2) {
+            nodes.add(1 / Math.sqrt(3));
+            weights.add(1.0);
+            nodes.add(-1 / Math.sqrt(3));
+            weights.add(1.0);
+        } else if (numberOfIntegrationPoints == 3) {
+            nodes.add(Math.sqrt(3.0 / 5.0));
+            weights.add(5.0 / 9.0);
+            nodes.add(0.0);
+            weights.add(8.0 / 9.0);
+            nodes.add(-Math.sqrt(3.0 / 5.0));
+            weights.add(5.0 / 9.0);
+        } else if (numberOfIntegrationPoints == 4) {
+            nodes.add(-0.861136);
+            weights.add(0.347855);
+            nodes.add(-0.339981);
+            weights.add(0.652145);
+            nodes.add(0.339981);
+            weights.add(0.652145);
+            nodes.add(0.861136);
+            weights.add(0.347855);
+        } else if (numberOfIntegrationPoints == 5) {
+            nodes.add(-0.906180);
+            weights.add(0.236927);
+            nodes.add(-0.538469);
+            weights.add(0.478629);
+            nodes.add(0.0);
+            weights.add(0.568889);
+            nodes.add(0.538469);
+            weights.add(0.478629);
+            nodes.add(0.906180);
+            weights.add(0.236927);
+        } else {
+            nodes = null;
+            weights = null;
+            System.out.println("Invalid n");
+        }
+
+        Point[] nodePoints = new Point[nodes.size() * nodes.size()];
+        Point[] weightPoints = new Point[weights.size() * weights.size()];
+
+        int counter = 0;
+        for(int i = 0; i < nodes.size(); i++){
+            for(int j = 0; j < nodes.size(); j++){
+                nodePoints[counter] = new Point(nodes.get(i), nodes.get(j));
+                weightPoints[counter] = new Point(weights.get(i), weights.get(j));
+                counter++;
+            }
+        }
+
+        Container container = new Container(weightPoints, nodePoints);
+        return container;
+    }
+
     public static double[][] calculateH(int index, double[][] nOverX, double[][] nOverY, Point[] weights,double determinant, double conductivity){
         double[][] yRow = Matrix.getRow(index, nOverY);
         double[][] yCol = Matrix.replace2dArrayDimensions(yRow);
@@ -88,6 +149,7 @@ public class Main {
         H = Matrix.multiplyNumberBy2dArray(weights[index].x * weights[index].y, H);
         return H;
     }
+
     public static double integerQuadrature(int N, double upperBoundary, double lowerBoundary, FunctionalInterface functionalInterface){
         double det = (upperBoundary - lowerBoundary) / 2;
         List<Double> nodes = new ArrayList<>();
@@ -132,7 +194,7 @@ public class Main {
         Grid grid;
         String fileName = "test.txt";
 
-        int numberOfIntegrationPoints = 5;//liczba punktow calkowania
+        int numberOfIntegrationPoints = 2;//liczba punktow calkowania
         int nDSF= 4;//liczba funkcji ksztaltu(nazwa zmiennej do zmiany!!!)
 
         DataImporter dataImporter = new DataImporter();
@@ -189,6 +251,66 @@ public class Main {
             grid.getElements()[resultElementCounter].setH(resultH);//zapisywanie do elementow
             equationsSystem.add(grid.getElements()[resultElementCounter]);//wczytywanie do ukladu rownan
         }
-        Matrix.print2dArray(equationsSystem.getHG());
+        //Matrix.print2dArray(equationsSystem.getHG());
+
+/*        container = calculateNodesAndWeights(numberOfIntegrationPoints);
+        weights = container.getWeights();
+        nodes = container.getNodes();
+        universalElement = new UniversalElement(nodes, nDSF);*/
+
+        weights = container.getWeights();
+        nodes = container.getNodes();
+
+        //DALA JEDNEJ SCIANY KTORA JEST SCIANA BRZEGOWA
+        Point[] dummyPoints = {new Point(-1,0.5773), new Point(-1,-0.5773)};//0.7886
+        double[] dummyWeights = {1 ,1};//0.7886
+        Point[] dummyNodes = {new Point(0,0), new Point(0.025,0), new Point(0.025,0.025), new Point(0,0.025)};
+        double[][] nArray = new double[dummyPoints.length][nDSF];
+
+        double detJ = MathFunctions.distance(dummyNodes[0], dummyNodes[1]) / 2.0;
+        double alfa = 25.0;
+        int numberOfIntegrationPointsOnSide = dummyPoints.length;// to trzeba zmienic tak zeby ustalac z mienna i na tej podstawie ustawai sie od[pwiednia ilosc punkow calkowania w tabeli
+
+        double [][] HBC = new double[nDSF][nDSF];
+
+        for (int i = 0; i < numberOfIntegrationPointsOnSide; i++) {//lece po jednym boku
+            double ksi = dummyPoints[i].x;
+            double eta = dummyPoints[i].y;
+            for (int j = 0; j < nDSF; j++) {
+                if (j == 0) {
+                    nArray[i][j] = 0.25 * (1 - ksi) * (1 - eta);
+                } else if (j == 1) {
+                    nArray[i][j] = 0.25 * (1 + ksi) * (1 - eta);
+                } else if (j == 2) {
+                    nArray[i][j] = 0.25 * (1 + ksi) * (1 + eta);
+                } else if (j == 3) {
+                    nArray[i][j] = 0.25 * (1 - ksi) * (1 + eta);
+                }
+            }
+
+            double [][] row = Matrix.getRow(i,nArray);
+            double [][] transRow = Matrix.replace2dArrayDimensions(Matrix.getRow(i,nArray));
+
+            HBC = Matrix.add2dArrays(HBC, Matrix.multiplyNumberBy2dArray(detJ, Matrix.multiplyNumberBy2dArray(alfa * dummyWeights[i], Matrix.multiply2dArrays(transRow, row))));
+        }
+        Matrix.print2dArray(HBC);//dobrze wychodzi
+        //tera trzeba powtorzyc dla innych bokow
+
+
+        /*  BUG!!! DEL WHEN FIXED
+        double [][] row = Matrix.getRow(0,nArray);
+        Matrix.print2dArray(row);
+        double [][] transRow = Matrix.replace2dArrayDimensions(Matrix.getRow(0,nArray));
+        Matrix.print2dArray(transRow);
+        Matrix.print2dArray(Matrix.replace2dArrayDimensions(transRow));
+        //tu jest blaad dzial tylko z row -> col, a z col -> row to tak srednio
+        //+
+        //bug w funkcji do transponowania (replace2dArrayDimensions) w Matrix
+        */
+
+
+
+
+
     }
 }
